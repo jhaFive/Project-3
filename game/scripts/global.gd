4d9f4371e6a4b0119a2bf2d75988b9653af16373
HeitorDragon1 <heitoroliveirahamamoto@hotmail.com>
2025-04-16 21:00:35 -0400

    added global variables for the ingredients

--- game/scripts/global.gd
+++ game/scripts/global.gd
@@ -1,3 +1,8 @@
 extends Node
 
 var is_dragging = false 
+
+#cup variables
+var boba = 'bo0' #0 = black, 1 = star, 2 = jelly
+var tea = 'te0' #0 = matcha, 1 = strawberry, 2 = brown
+var top = 'to0' #0 = cream, 1 = foam, 2 = berries
