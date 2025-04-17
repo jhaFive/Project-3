745d52dee39196a0bafe0ec4e5df52503aa81288
jhaFive <jha5@kent.edu>
2025-04-17 11:47:38 -0400

    Reimplemented Some Elements and Exported New Build

--- game/scripts/Drink_Stack.gd
+++ game/scripts/Drink_Stack.gd
@@ -4,9 +4,17 @@ signal reset
 
 @onready var clear_button = get_parent().find_child("ClearButton")
 @onready var dropzone = $dropzone
+
+# These variables keep track of what can be dragged in the cup
 @onready var new_pearl = $NewPearl
 @onready var star = $Star
 @onready var jelly = $Jelly
+@onready var matcha = $Matcha
+@onready var strawberry = $Strawberry
+@onready var brownSugar = $BrownSugar
+@onready var whipCream = $WhipCream
+@onready var cheeseFoam = $CheeseFoam
+@onready var strawberries = $Strawberries
 
 var has_cup: bool = false
 
@@ -20,9 +28,22 @@ var topping_available: bool = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	clear_button.clear_ingredients.connect(_clear_drink)
+	
+	# These update the graphics and keeps track of what is dragged to the cup
+	# This section handles the boba ingredients
 	new_pearl.pearl_delivered.connect(_update_drink)
 	star.star_delivered.connect(_update_drink)
 	jelly.jelly_delivered.connect(_update_drink)
+	
+	# This section handles the tea ingredients
+	matcha.matcha_delivered.connect(_update_drink)
+	strawberry.strawberry_delivered.connect(_update_drink)
+	brownSugar.brownSugar_delivered.connect(_update_drink)
+	
+	# This section handles the topping ingredients
+	whipCream.whipCream_delivered.connect(_update_drink)
+	cheeseFoam.cheeseFoam_delivered.connect(_update_drink)
+	strawberries.strawberries_delivered.connect(_update_drink)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -44,9 +65,11 @@ func _update_drink(type: String, ingredient: String):
 
 func _clear_drink():
 	# where tf is this signal being recived?
+	# I'm not entirely sure, I'll be sure to ask about this part
 	emit_signal("reset")
 	
 	# Why are we using strings for a variable that will only hold 1 value at a time again?
+	# I believe strings are being used here to be more understandable that there is nothing in the cup. Using strings, I more easily understand what is the current item
 	Global.current_boba = str("NOTHING")
 	Global.current_tea = str("NOTHING")
 	Global.current_topping = str("NOTHING")
