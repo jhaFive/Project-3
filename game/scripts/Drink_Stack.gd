3e52be49cec30932a98cc053c6a6ce14741946dc
HeitorDragon1 <heitoroliveirahamamoto@hotmail.com>
2025-04-17 05:41:17 -0400

    Add files via upload

--- game/scripts/Drink_Stack.gd
+++ game/scripts/Drink_Stack.gd
@@ -7,40 +7,22 @@ signal reset
 @onready var new_pearl = $NewPearl
 @onready var star = $Star
 @onready var jelly = $Jelly
-@onready var matcha = $Matcha
-@onready var strawberry = $Strawberry
-@onready var brownSugar = $BrownSugar
-@onready var whipCream = $WhipCream
-@onready var cheeseFoam = $CheeseFoam
-@onready var strawberries = $Strawberries
 
 var has_cup: bool = false
 
-var current_boba: String
-var current_tea: String
-var current_topping: String
+# variables deleted as they have been converted to global
+# we will need them in global to be able to change their values between all scripts at the same time
 
 var boba_available: bool = true
 var tea_available: bool = true
 var topping_available: bool = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	clear_button.clear_ingredients.connect(_clear_drink)
-	
-	# boba
 	new_pearl.pearl_delivered.connect(_update_drink)
 	star.star_delivered.connect(_update_drink)
 	jelly.jelly_delivered.connect(_update_drink)
-	
-	# tea
-	matcha.matcha_delivered.connect(_update_drink)
-	strawberry.strawberry_delivered.connect(_update_drink)
-	brownSugar.brownSugar_delivered.connect(_update_drink)
-	
-	# topping
-	whipCream.whipCream_delivered.connect(_update_drink)
-	cheeseFoam.cheeseFoam_delivered.connect(_update_drink)
-	strawberries.strawberries_delivered.connect(_update_drink)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -48,22 +30,27 @@ func _process(delta: float) -> void:
 
 func _update_drink(type: String, ingredient: String):
 	if type == "boba":
-		current_boba = ingredient
+		Global.current_boba = ingredient
 		boba_available = false
 	if type == "tea":
-		current_tea = ingredient
+		Global.current_tea = ingredient
 		tea_available = false
 	if type == "topping":
-		current_topping = ingredient
+		Global.current_topping = ingredient
 		topping_available = false
 	
+	# debugg
 	print(ingredient)
 
 func _clear_drink():
+	# where tf is this signal being recived?
 	emit_signal("reset")
-	current_boba = str("NOTHING")
-	current_tea = str("NOTHING")
-	current_topping = str("NOTHING")
+	
+	# Why are we using strings for a variable that will only hold 1 value at a time again?
+	Global.current_boba = str("NOTHING")
+	Global.current_tea = str("NOTHING")
+	Global.current_topping = str("NOTHING")
+	
 	boba_available = true
 	tea_available = true
 	topping_available = true
--- game/scripts/global.gd
+++ game/scripts/global.gd
@@ -2,7 +2,15 @@ extends Node
 
 var is_dragging = false 
 
-#cup variables
-var boba = 'bo0' #0 = black, 1 = star, 2 = jelly
-var tea = 'te0' #0 = matcha, 1 = strawberry, 2 = brown
-var top = 'to0' #0 = cream, 1 = foam, 2 = berries
+# ingredients variables
+# These work as variables for the curent order as they (should) get updated everytime a new order is made 
+# in the "res://scripts/levels/level1.gd" script there is a string with the 3 values for some reason
+var boba = 'bo0' # Replace number to get another ingredient 0 = black, 1 = star, 2 = jelly
+var tea = 'te0' # Replace number to get another ingredient 0 = matcha, 1 = strawberry, 2 = brown
+var top = 'to0' # Replace number to get another ingredient 0 = cream, 1 = foam, 2 = berries
+
+# ingredients on cup variables
+# why strings?
+var current_boba: String
+var current_tea: String
+var current_topping: String
